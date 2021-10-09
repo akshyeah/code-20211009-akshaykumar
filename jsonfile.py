eaf7977b3fd310025b46c07d4e7d2c1bb7a43f4d
Akshay kumar <akshay1.kumar@lntinfotech.com>
2021-10-09 17:34:11 +0530

    updated jsonfile.py

--- jsonfile.py
+++ jsonfile.py
@@ -7,11 +7,6 @@ def read(filename):
 
 def write(filename="datatest.json", newdata=[{"Kindly Provide the data"}]):
     with open (filename, mode="w") as jfile:
-        #filedata = json.load(jfile)
-        #jfile.write(newdata["Gender"])
-        #filedata.append(newdata)
-        #print(filedata)
-        #jfile.seek(0)
         json.dump(newdata, jfile, indent=1, sort_keys=True)
 
     jfile.close()
